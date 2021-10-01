decd6260cfdbe62d6be3de1fcaef94c14e164864
Bailey Lievens <baileylievens@hotmail.be>
2021-10-01 21:12:25 +0200

    Feat: Added profile positioning

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -12,6 +12,27 @@
 <body>
     @include('partials/navigation')
 
+    <section class="profile_section">
+        <img class="profile_picture" src="{{ asset('images/test.jpg') }}" alt="Profile picture">
+
+        <div class="user_info_container">
+            
+            <h2>Bailey Lievens</h2>
+            <p>This is a cool human. Fo sho yess sir, no cap. No 
+                goddamn doubt in my mind this guy is straight up fire. Maybe
+                the fire might be too hot for this world? Global warming? Probably
+                this guy.
+            </p>
+
+            <h3>Top owned NFT's</h3>
+            <div class="top_nft_container">
+                <img src="{{ asset('images/test.jpg') }}" alt="#1 owned nft">
+                <img src="{{ asset('images/test.jpg') }}" alt="#2 owned nft">
+                <img src="{{ asset('images/test.jpg') }}" alt="#3 owned nft">
+            </div>
+
+        </div>
+    </section>
     
     @include('partials/footer')
 </body>
