0e8b66372c0bd4e284e884acd9c3830f8891c6a2
nickbevers <nick.bevers9@gmail.com>
2021-10-16 18:35:50 +0200

    fix: Changes some Routes, and changes the function on the routes to be more advanced

--- routes/web.php
+++ routes/web.php
@@ -2,31 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CollectionController;
 use App\Http\Controllers\NftController;
 
-Route::get('/', function () {
-    return view('index');
-});
 
-Route::get('/login', function () {
-    return view('login');
-});
+//root directory
+Route::get('/', function () {return view('index');});
+Route::get('/discover', function () {return view('discover');});
 
-Route::get('/signup', function () {
-    return view('signup');
-});
+//Login related
+Route::get('/login', function () {return view('login');});
+Route::get('/signup', function () {return view('signup');});
 
+//Nft related
 Route::get('/detail/{nft_name}', [NftController::class, 'showDetail']);
+Route::get('/upload', function() {return view('upload');});
 
-                        //{username} --> variable name in the url that you get access to in the corresponding function, in this example showCollection
-Route::get('/collection/{username}', [UserController::class, 'showCollection']);
+//Collection-related
+Route::get('/collection/create', [CollectionController::class, 'create']);
+//{username} --> variable name in the url that you get access to in the corresponding function, in this example showCollection
+Route::get('/collection/{username}', [CollectionController::class, 'show']);
+Route::post('/collection/store', [CollectionController::class, 'store']);
 
+//User related
 Route::get('/user', [UserController::class, 'index'] ); //Look at the UserController to see what function to call, in this case function index
-
-Route::get('/discover', function () {
-    return view('discover');
-});
-
-Route::get('/upload', function() {
-    return view('upload');
-});
