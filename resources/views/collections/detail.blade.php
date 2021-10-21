dc72b7916351db5b2c57674bdee4abdcadc87e15
nickbevers <nick.bevers9@gmail.com>
2021-10-21 22:19:24 +0200

    feat: Adds functionality so that an nft in a collection can be removed, edited or removed from the collection

--- resources/views/collections/detail.blade.php
+++ resources/views/collections/detail.blade.php
@@ -11,20 +11,24 @@
                 Created by: {{ $collection->user->first_name . " " .  $collection->user->last_name }}
             </p>
 
-            <a href="../edit/{{ $collection->title }}" style="height: 1.1em">Edit this Collection</a><br><br>
-            <a href="../remove/{{ $collection->id }}" style="height: 1.1em">Remove this Collection</a><br><br>
+            <a href="/collection/edit/{{ $collection->id }}" style="height: 1.1em">Edit this Collection</a><br><br>
+            <a href="/collection/add/{{ $collection->id }}" style="height: 1.1em">Add an NFT this Collection</a><br><br>
+            <a href="/collection/remove/{{ $collection->id }}" style="height: 1.1em; margin-bottom:30px">Remove this Collection</a><br><br>
 
-            <div class="buy_add_container">
-                <a href="#">Buy</a>
-            </div>
         </div>
+
+        {{-- Adding to a collection -> Get list of all personal NFT's, on click on a NFT -> set that nft's collection id to the collection id given to the page --}}
 
         <div class="nfts">
             @foreach($nfts as $nft)
-                <a href="./detail/{{ $collections->id }}">
-                    <img src="{{ asset('images/test.jpg') }}" alt="art for the nft {{ $collections->id }}">
-                    <h2>{{ $collections->title }}</h2>
+                <a href="/nft/detail/{{ $nft->id }}">
+                    <img src="{{ asset('images/test.jpg') }}" alt="art for the nft {{ $nft->title }}" width="250px" style="margin-bottom: 0">
+                    <p>{{ $nft->title }}</p>
+                    <a href="/nft/edit/{{ $nft->id }}" style="height: 1.1em">Edit this NFT</a><br>
+                    <a href="/nft/remove/{{ $nft->id }}" style="height: 1.1em">Remove this NFT</a><br>
+                    <a href="/nft/update/{{ $nft->id }}" style="height: 1.1em">Remove from this Collection</a><br><br><br><br><br>
                 </a>
+
             @endforeach
         </div>
     </section>
