d89e56a4afc4c6ff828df96cfad10e363397190c
nickbevers <nick.bevers9@gmail.com>
2021-10-22 22:26:36 +0200

    feat: Adds views for the wallet page (all nfts of a user)

---
+++ resources/views/wallet/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts/app')
+
+@section('content')
+<!-- Collection -->
+    @include('../partials/navigation')
+    <section class="">
+        <h1>All of {{$user}} NFT's</h1>
+    </section>
+
+    <section class="nft_showcase">
+        @foreach($nfts as $nft)
+            <div>
+                <a href="/nft/detail/{{$nft->id}}" style="margin-bottom: 0">
+                    <img src="{{ asset('images/test.jpg') }}" alt="art for the nft {{ $nft->title }}" width="250px" style="margin-bottom: 0">
+                    <p>{{ $nft->title }}</p>
+                </a>
+                @if($nft->for_sale == 0)
+                    <p style="width:20%; padding: 0; margin: 0; margin-left:40%;">This NFT cannot be bought right now</p>
+                @endif
+            </div>
+            @endforeach
+    </section>
+    
+    <script src="{{ asset('js/slide_menu.js') }}"></script>
+@endsection
