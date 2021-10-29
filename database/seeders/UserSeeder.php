bf16c40dfef850cf9b84219a6ddfa02bdb61527a
nickbevers <nick.bevers9@gmail.com>
2021-10-29 12:35:38 +0200

    fix: removes the quotes from the hash function so that the password is stored correctly when seeding

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -30,7 +30,7 @@ class UserSeeder extends Seeder
         $user->first_name = "Ilya";
         $user->last_name = "Plyusnin";
         $user->email = "i@i.i";
-        $user->password = "Hash::make('a')"; //Password is 'a' without quotes
+        $user->password = Hash::make('a'); //Password is 'a' without quotes
         $user->wallet = "1";
         $user->profile_picture = "https://res.cloudinary.com/doafzvgjh/image/upload/v1634674189/unmk6hbinwxqmqyumgvs.png";
         $user->save();
@@ -39,7 +39,7 @@ class UserSeeder extends Seeder
         $user->first_name = "Bailey";
         $user->last_name = "Lievens";
         $user->email = "b@b.b";
-        $user->password = "Hash::make('a')"; //Password is 'a' without quotes
+        $user->password = Hash::make('a'); //Password is 'a' without quotes
         $user->wallet = "2";
         $user->profile_picture = "https://res.cloudinary.com/doafzvgjh/image/upload/v1634674526/bxpcjyf39tbutp1myt0u.png";
         $user->save();
@@ -48,7 +48,7 @@ class UserSeeder extends Seeder
         $user->first_name = "Ellen";
         $user->last_name = "Hiel";
         $user->email = "e@e.e";
-        $user->password = "Hash::make('a')"; //Password is 'a' without quotes
+        $user->password = Hash::make('a'); //Password is 'a' without quotes
         $user->wallet = "3";
         $user->profile_picture = "https://res.cloudinary.com/doafzvgjh/image/upload/v1634674518/mvornfn9rl8lno3krbhn.png";
         $user->save();
@@ -57,7 +57,7 @@ class UserSeeder extends Seeder
         $user->first_name = "Nick";
         $user->last_name = "Bevers";
         $user->email = "n@n.n";
-        $user->password = "Hash::make('a')"; //Password is 'a' without quotes
+        $user->password = Hash::make('a'); //Password is 'a' without quotes
         $user->wallet = "4";
         $user->profile_picture = "https://res.cloudinary.com/doafzvgjh/image/upload/v1634674336/lalteyjlxphogk72fxeq.png";
         $user->save();
