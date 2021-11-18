4b95d6f53f4d803838c3111120dc7908f8e5bf5a
ellenhiel <elhi12@hotmail.com>
2021-11-18 18:34:02 +0100

    added heart svg

--- resources/views/discover.blade.php
+++ resources/views/discover.blade.php
@@ -58,6 +58,7 @@
                 <a href="/nft/detail/{{ $nft->id }}">
                     <img src="{{ asset('images/test.jpg') }}" alt="{{ $nft->title }}" data-id="{{ $nft->id }}">
                     <p>{{ $nft->title }}</p>
+                    <a class="heart" href="#"><svg class="unfavourite" xmlns="http://www.w3.org/2000/svg" width="22" height="20" viewBox="0 0 24 24"><path d="M12 4.419c-2.826-5.695-11.999-4.064-11.999 3.27 0 7.27 9.903 10.938 11.999 15.311 2.096-4.373 12-8.041 12-15.311 0-7.327-9.17-8.972-12-3.27z"/></svg></a>
                 </a> 
             </div>
         @endforeach
