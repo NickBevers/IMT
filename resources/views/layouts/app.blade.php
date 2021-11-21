7349ce867f07ea4d608728de3c0f2b5fa592eadc
nickbevers <nick.bevers9@gmail.com>
2021-11-21 19:20:45 +0100

    feat: adds the dependency for ethers.js to the layout file; adds script tag to the discover page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? '' }}</title>
-    @livewireStyles
+    
     <!-- Font import -->
     <style>
     @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
@@ -14,8 +14,10 @@
     <link rel="stylesheet" href="{{ asset('css/general.css') }}">
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
     <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+    <script src="https://cdn.ethers.io/lib/ethers-5.2.umd.min.js" type="application/javascript"></script>
 </head>
 <body>
+    @livewireStyles
     @if (!Auth::check() && !Request::is('discover') && !Request::is('/') && !Request::is('login') && !Request::is('signup'))
         <script>window.location = "/login";</script>
     @endif
