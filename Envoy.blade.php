8249bf9110d23ab1373e5707aa38deee6d39afaf
Bailey Lievens <baileylievens@hotmail.be>
2021-11-24 17:27:55 +0100

    test: bgcolor red

---
+++ Envoy.blade.php
@@ -0,0 +1,23 @@
+@servers (['production' => 'deploybot@139.162.131.215'])
+
+@task(‘deploy’, [‘on’ => ‘production’])
+5AZZpVpTrWRmM2Wz$M98
+cd /home/deploybot/app/imt/
+php artisan down
+git reset --hard HEAD
+git pull origin master
+composer install
+php artisan migrate --force
+php artisan up
+@endtask
+
+@task(‘deploybeta’, [‘on’ => ‘production’])
+5AZZpVpTrWRmM2Wz$M98
+cd /home/deploybot/app/imt/
+php artisan down
+git reset --hard HEAD
+git pull origin master
+composer install
+php artisan migrate --force
+php artisan up
+@endtask
