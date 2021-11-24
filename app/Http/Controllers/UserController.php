64cdbe15f600149147f21b041dedf0893b27512e
Bailey Lievens <baileylievens@hotmail.be>
2021-11-24 23:22:30 +0100

    Feat: can remove profile img

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -83,6 +83,7 @@ class UserController extends Controller
         $user = Auth::user();
 
         $uploadedFileUrl = Cloudinary::upload($request->file("profilePicture")->getRealPath())->getSecurePath();
+        //TODO: Update img instead of uploading a new image
 
         $user->first_name = $request['firstname'];
         $user->last_name = $request['lastname'];
