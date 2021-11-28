17ee389aca72312fa34000574f3aa5f5b84199eb
nickbevers <nick.bevers9@gmail.com>
2021-11-28 17:13:47 +0100

    feat: Adds getPrice and isForSale to the frontend; test: first attempt at putUpForSale and BuyNFT function (not working due to possible contract error)

--- resources/views/nfts/detail.blade.php
+++ resources/views/nfts/detail.blade.php
@@ -10,12 +10,46 @@
                 Created by: {{ $user->first_name . " " .  $user->last_name }}
             </p>
             @if($nft->for_sale == 0)
-                <p>This NFT cannot be bought right now</p>
+                <p class="forSale">This NFT cannot be bought right now</p>
             @endif
             <div class="price_wrapper">
                 <div class="eth_wrapper">
                     <img src="{{ asset('images/ethIcon.png') }}" alt="Etherium icon">
-                    <h3>{{ $nft->price }}</h3>
+                    {{-- <h3>{{ $nft->price }}</h3> --}}
+                    <h3 class="price"></h3>
+                    <script type="text/javascript">
+                        window.addEventListener("load", async()=>{
+                            const provider = new ethers.providers.Web3Provider(window.ethereum);
+                            const signer = provider.getSigner();
+                            const contractAddress = "0x76d463D9CA4CAE1Fd478d62e9914A6b6Cc2b604e";
+                            let Abi;
+                            await fetch("/abi/contract.json").then((res) => {return res.json();}).then((data) => {Abi = data;});
+                            const contract = new ethers.Contract(contractAddress, Abi, provider);
+                            let contractWithSigner = contract.connect(signer);
+                            
+                            if({{$nft->minted}} == true){
+                                console.log("MINTED");
+                                const priceField = document.querySelector(".price");
+                                const price = await contractWithSigner.getPrice("{{$nft->token_id}}");
+                                const isForSale = await contractWithSigner.isForSale("{{$nft->token_id}}");
+                                // console.log(ethers.utils.toString(price));
+                                console.log(ethers.utils.formatEther(price));
+                                console.log(isForSale);
+                                if (isForSale === true){
+                                    document.querySelector(".forSale").style.visibility = 'hidden';
+                                }
+                                if (isForSale === false){
+                                    document.querySelector(".forSale").style.visibility = 'visible';
+                                }
+                                priceField.innerHTML =  price;
+                            }
+
+                            else{
+                                console.log("NOT MINTED");
+                            }
+                            
+                        });
+                    </script>
                 </div>
                 <div class="conversion_wrapper">
                     <h3>€{{ $nft->convertedPrice["EUR"] }}</h3>
@@ -28,18 +62,23 @@
                 <a class="buy_btn">Buy</a>
                 @if($nft->minted == false)
                     <a class="buy_btn mint_btn" style="cursor: pointer">Mint</a>
-
-                    <script type="text/javascript">
+                @endif
+                <a class="sell_btn">Sell</a>
+                
+                <script type="text/javascript"> 
+                    if ({{$nft->minted}} == false){
                         const mintNFTBtn = document.querySelector(".mint_btn");
                         mintNFTBtn.addEventListener("click", async()=>{
-                            // console.log("MINTED");
                             const provider = new ethers.providers.Web3Provider(window.ethereum);
-                            const signer = provider.getSigner();
+                            const signer = await provider.getSigner();
                             const contractAddress = "0x76d463D9CA4CAE1Fd478d62e9914A6b6Cc2b604e";
                             let Abi;
-                            await fetch("/abi/contract.json").then((res) => {return res.json();}).then((data) => {Abi = data; console.log(Abi);});
-                            const contract = new ethers.Contract(contractAddress, Abi, provider);
-                            let contractWithSigner = contract.connect(signer);
+                            await fetch("/abi/contract.json").then((res) => {return res.json();}).then((data) => {Abi = data;});
+                            const contract = await new ethers.Contract(contractAddress, Abi, provider);
+                            let contractWithSigner = await contract.connect(signer);
+
+
+                            // console.log("MINTED");
                             let media_file = "https://ipfs.io/ipfs/{{$nft->media_url}}";
                             // let price = ethers.utils.parseEther({{$nft->price}}.toString());
                             let tempPrice = "{{$nft->price}}";
@@ -48,18 +87,20 @@
                             console.log(price);
         
                                                 
-                            const response =  await contractWithSigner.mintNFT(media_file, price);
-                            const itemId = response['hash'];
+                            const mintResponse =  await contractWithSigner.mintNFT(media_file, price);
+                            const itemId = mintResponse['hash'];
                             const nftId =  "{{$nft->id}}";
+                            const nft_owner = mintResponse['from'];
         
                             const form = document.createElement('form');
                             let nft_id = nftId;
                             let item_id = itemId;
+                            let owner = nft_owner;
                             form.method = "POST";
-                            form.action = `/nft/addItemId/${nft_id}/${itemId}`;
+                            form.action = `/nft/addItemId/${nft_id}/${itemId}/${owner}`;
         
         
-                            let csrf_token = "{{csrf_token()}}"
+                            let csrf_token = "{{csrf_token()}}";
                             const hiddencsrf = document.createElement('input');
                             hiddencsrf.type = 'hidden';
                             hiddencsrf.name = "_token";
@@ -70,9 +111,48 @@
                             document.body.appendChild(form);
                             form.submit();
                         });
-                    </script>
-                @endif
-                
+                    }
+
+                    if ({{$nft->minted}} == true){
+                        const buyNFTBtn = document.querySelector(".buy_btn");
+                        buyNFTBtn.addEventListener("click", async()=>{
+                            const provider = new ethers.providers.Web3Provider(window.ethereum);
+                            const signer = await provider.getSigner();
+                            const contractAddress = "0x76d463D9CA4CAE1Fd478d62e9914A6b6Cc2b604e";
+                            let Abi;
+                            await fetch("/abi/contract.json").then((res) => {return res.json();}).then((data) => {Abi = data;});
+                            const contract = await new ethers.Contract(contractAddress, Abi, provider);
+                            let contractWithSigner = await contract.connect(signer);
+                        
+                            const buyResponse =  await contractWithSigner.buyNFT("{{$nft->token_id}}");
+                            console.log(buyResponse);
+                        });                        
+                    }
+
+                    const sellNFTBtn = document.querySelector(".sell_btn");
+                    sellNFTBtn.addEventListener("click", async()=>{
+                        await ethereum.request({ method: "eth_requestAccounts" });
+                        // let provider = ethers.getDefaultProvider("rinkeby", "{{env('ETHERSCAN_API')}}");
+                        
+                        let provider = new ethers.providers.Web3Provider(window.ethereum);
+                        console.log(provider);
+                        let signer = await provider.getSigner();
+                        console.log(signer);
+                        let contractAddress = "0x76d463D9CA4CAE1Fd478d62e9914A6b6Cc2b604e";
+                        let Abi;
+                        await fetch("/abi/contract.json").then((res) => {return res.json();}).then((data) => {Abi = data;});
+                        let contract = await new ethers.Contract(contractAddress, Abi, provider);
+                        let contractWithSigner = await contract.connect(signer);
+
+                        let price = ethers.utils.parseUnits("{{$nft->price}}", "ether");
+                        
+                        console.log(price);
+                        const buyResponse =  await contractWithSigner.putUpForSale("{{$nft->token_id}}", price);
+                        console.log("RESPONSE: " + buyResponse);
+                    });
+
+                </script>
+
             </div>
         </div>
     </section>
